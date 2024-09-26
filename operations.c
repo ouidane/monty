15247931076051bdb2bc74ad39da5f232e192d93
ouidane <ouidanezakaria@gmail.com>
2024-09-26 01:40:32 +0100

    update sub op

--- operations.c
+++ operations.c
@@ -31,21 +31,27 @@ void add_op(stack_t **stack, unsigned int nline)
   *
   * Return: NAIN
   */
-void sub_op(stack_t **stack, unsigned int nline)
+void sub_op(stack_t **head, unsigned int nline)
 {
-	stack_t *temp;
+	stack_t *aux;
+	int sus, nodes;
 
-	if (!(stack) || !(*stack)->next)
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", nline);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-
-	temp = *stack;
-	(*stack)->next->n -= (*stack)->n;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
 }
 
 
